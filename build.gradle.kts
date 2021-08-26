7a0f0eff9a37c51789a4c98219d67f0f9afca99c
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2021-08-26 16:57:24 +0900

    org.springframework.boot:spring-boot-starter-web 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.github.microutils:kotlin-logging:1.12.5") // Logging
+    implementation("org.springframework.boot:spring-boot-starter-web") // MVC
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
