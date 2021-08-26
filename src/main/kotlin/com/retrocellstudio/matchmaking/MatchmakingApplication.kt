ef3d7f7fef748defb2c935401fb319cd654e25a1
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2021-08-26 14:42:30 +0900

    Initial commit

---
+++ src/main/kotlin/com/retrocellstudio/matchmaking/MatchmakingApplication.kt
@@ -0,0 +1,11 @@
+package com.retrocellstudio.matchmaking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatchmakingApplication
+
+fun main(args: Array<String>) {
+    runApplication<MatchmakingApplication>(*args)
+}
