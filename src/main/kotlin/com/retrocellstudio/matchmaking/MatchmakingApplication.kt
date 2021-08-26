5495822c60f0e8a043a77f671d5f219238b1cbda
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2021-08-26 16:00:05 +0900

    SLF4J를 wrapping한 kotlin-logging 적용 (info이상 레벨만 콘솔 출력)

--- src/main/kotlin/com/retrocellstudio/matchmaking/MatchmakingApplication.kt
+++ src/main/kotlin/com/retrocellstudio/matchmaking/MatchmakingApplication.kt
@@ -1,9 +1,12 @@
 package com.retrocellstudio.matchmaking
 
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import javax.annotation.PostConstruct
+
+private val logger = KotlinLogging.logger {}
 
 @SpringBootApplication
 class MatchmakingApplication {
@@ -18,4 +21,10 @@ class MatchmakingApplication {
 
 fun main(args: Array<String>) {
     runApplication<MatchmakingApplication>(*args)
+
+    logger.trace("trace test")
+    logger.debug { "debug test" }
+    logger.info { "info test" }
+    logger.warn ("warn test")
+    logger.error { "error test" }
 }
