31a8a065493fbef0b72bb6a45e743cf23af08aca
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2021-08-26 15:03:44 +0900

    profile yml로 적용

--- src/main/kotlin/com/retrocellstudio/matchmaking/MatchmakingApplication.kt
+++ src/main/kotlin/com/retrocellstudio/matchmaking/MatchmakingApplication.kt
@@ -1,10 +1,20 @@
 package com.retrocellstudio.matchmaking
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class MatchmakingApplication
+class MatchmakingApplication {
+    @Value("\${matchmaking.title-message}")
+    val titleMessage: String = ""
+
+    @PostConstruct
+    fun printTitleMessage() {
+        println(titleMessage);
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<MatchmakingApplication>(*args)
