887f412894326e93e508b130c5074de890efd2d6
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2021-08-30 18:06:52 +0900

    spring data entity 관련 에러 제거용 플러그인

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.3.61" // entity 에러 제거용
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.spring") version "1.5.21"
 }
